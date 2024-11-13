29e6a08ad44431832223cee0a673b9af51107edf
Pboliop <pboliop@gmail.com>
2024-11-13 17:50:46 -0600

    refactor the filters

--- Codigos/Filtrado2.py
+++ Codigos/Filtrado2.py
@@ -4,10 +4,20 @@ from scipy import signal
 import os
 import math
 
+order = 4
+file_count = 19
+chunk_size = 128
+low_cutoff = 8/64
+high_cutoff = 16/64
+sampling_rate = 128
 
 columns_to_use = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 column_names = ['AF3', 'F7', 'F3', 'FC5', 'T7', 'P7', 'O1', 'O2', 'P8', 'T8', 'FC6', 'F4', 'F8', 'AF4']
-chunk_size = 128
+
+file_prefix = 'S'
+input_dir = '/Users/emiliasalazar/INTELIGENCIA_ARTIFICIAL/EQUIPO_INTELIGENCIA_ARTIFICIAL/Grabaciones/'
+output_dir = '/Users/emiliasalazar/INTELIGENCIA_ARTIFICIAL/EQUIPO_INTELIGENCIA_ARTIFICIAL/Procesado/'
+
 
 def detect_repeated_values(data, threshold=38400):
     error_messages = []
@@ -53,33 +63,11 @@ def detect_repeated_values(data, threshold=38400):
 
     return tensor, error_messages
 
-
-
 def getFilter(order, low, high):
     return signal.butter(order, [low, high], 'band')
+def removeDCPassFilter(array, order, low, high, Fs):
 
-
-def removeDC(array):
-    newArray = np.full_like(array, 0)
-    second = 0
-
-    for i in range(len(array)):
-        for j in range(array.shape[1]):
-            meanA = np.mean(array[i][j])
-            newArray[i][j] = array[i][j] - meanA     
-    
-    for k in range(len(newArray)):
-        x = np.where(~newArray[second].any(axis=1))[0]
-        if x.size == 0:
-            second += 1
-        else:
-            newArray = np.delete(newArray, second, 0)
-                
-    return newArray
-
-
-def removeDCPassFilter(array, threshold, Fs):
-    b, a = signal.butter(1, threshold, 'high')
+    b, a = getFilter(order, low, high)
     newArray = np.full_like(array, 0)
     second = 0
     
@@ -97,26 +85,6 @@ def removeDCPassFilter(array, threshold, Fs):
         
     return newArray
 
-
-def apply_bandpass_filter(tensor, b, a):
-    filtered_tensor = np.zeros_like(tensor)
-    for i in range(len(tensor)):
-        for j in range(tensor.shape[1]):
-            filtered_tensor[i, j, :] = signal.filtfilt(b, a, tensor[i, j, :])
-    return filtered_tensor
-
-
-input_dir = '/Users/emiliasalazar/INTELIGENCIA_ARTIFICIAL/EQUIPO_INTELIGENCIA_ARTIFICIAL/Grabaciones/'
-output_dir = '/Users/emiliasalazar/INTELIGENCIA_ARTIFICIAL/EQUIPO_INTELIGENCIA_ARTIFICIAL/Procesado/'
-file_prefix = 'S'
-file_count = 19
-order = 4
-low_cutoff = 8
-high_cutoff = 32
-sampling_rate = 128
-b, a = getFilter(order, low_cutoff / (0.5 * sampling_rate), high_cutoff / (0.5 * sampling_rate))
-
-
 def process_file(input_path, output_path):
     data = pd.read_csv(input_path, usecols=columns_to_use, names=column_names, header=0, skipinitialspace=True)
     filtered_data = data.iloc[:, :]
@@ -131,10 +99,8 @@ def process_file(input_path, output_path):
         for message in error_messages:
             print(message)
         return 
-
-    tensor = removeDC(tensor)
-    bandpassed_tensor = apply_bandpass_filter(tensor, b, a)
-    final_tensor = removeDCPassFilter(bandpassed_tensor, threshold=0.16, Fs=sampling_rate)
+    final_tensor = removeDCPassFilter(tensor, order=4, low=low_cutoff, high=high_cutoff, Fs=sampling_rate)
+    
 
     flat_data = final_tensor.transpose(0, 2, 1).reshape(-1, final_tensor.shape[1])
 
@@ -142,8 +108,6 @@ def process_file(input_path, output_path):
     
     processed_df.to_csv(output_path, index=False)
     print(f"Processed data saved to {output_path}")
-
-
 
 for i in range(1, file_count + 1):
     input_path = os.path.join(input_dir, f"{file_prefix}{i}.csv")
